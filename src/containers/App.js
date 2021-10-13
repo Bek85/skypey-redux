e611be136c69742a876d06f34f95bd74bb187c57
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-14 00:52:10 +0500

    refactor app structure to have containers folder to indicate redux related components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
 import './App.css';
-import Main from './Main';
-import Sidebar from './Sidebar';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
 import store from '../store';
 
 const App = () => {
