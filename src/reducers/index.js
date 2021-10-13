08b3b9e86743916ffeb4a4955ca12b38272abca6
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-14 01:21:54 +0500

    use combineReducers function to combine seprate reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
-export default function reducer(state, action) {
-  return state;
-}
+import { combineReducers } from 'redux';
+import user from './userReducer';
+import contacts from './contactsReducer';
+
+export default combineReducers({
+  user,
+  contacts,
+});
