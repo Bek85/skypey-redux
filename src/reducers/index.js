e04e90df4286095999b150f50baebf531849658f
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-15 01:31:43 +0500

    implement state logic to typing reducer and addit to combineReducers function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import user from './user';
 import contacts from './contacts';
 import activeUserId from './activeUserId';
 import messages from './messages';
+import typing from './typing';
 
 export default combineReducers({
   user,
   contacts,
   activeUserId,
   messages,
+  typing,
 });
