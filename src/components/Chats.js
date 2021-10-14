4a493fbd50d103f667357832f5938eda6bf9f9f8
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-15 01:18:10 +0500

    render messages in Chats component

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Chats.css';
 
-const Chats = () => {
-  return <div>Chats</div>;
+const Chat = ({ message }) => {
+  const { text, is_user_msg } = message;
+  return (
+    <span className={`Chat ${is_user_msg ? 'is-user-msg' : ''}`}>{text}</span>
+  );
+};
+
+const Chats = ({ messages }) => {
+  return (
+    <div className="Chats">
+      {messages.map((message) => (
+        <Chat message={message} key={message.number} />
+      ))}
+    </div>
+  );
 };
 
 export default Chats;
