d54604a83cb92a51940b66cd8be879cdfaa47b14
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-14 13:23:37 +0500

    create constants for action-types and first action creator and dispatch this action in User component

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { setActiveUserId } from '../actions';
+import store from '../store';
 import './User.css';
 
 const User = ({ user }) => {
   const { name, profile_pic, status } = user;
 
+  const handleUserClick = ({ user_id }) => {
+    store.dispatch(setActiveUserId(user_id));
+  };
+
   return (
-    <div className="User">
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
       <img src={profile_pic} alt={name} className="User__pic" />
       <div className="User__details">
         <p className="User_details-name">{name}</p>
