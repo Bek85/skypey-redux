fedaf991f8394f644efca949246b83af4ce7a067
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-10-14 15:45:13 +0500

    create Chats component and prepare messages from staticdata to be used in Chats component

---
+++ src/components/Chats.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Chats.css';
+
+const Chats = () => {
+  return <div>Chats</div>;
+};
+
+export default Chats;
--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './ChatWindow.css';
+import _ from 'lodash';
 import store from '../store';
 import Header from '../components/Header';
+import Chats from '../components/Chats';
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
+  const activeMsgs = useMemo(
+    () => state.messages[activeUserId],
+    [state, activeUserId]
+  );
+  const messages = useMemo(() => _.values(activeMsgs), [activeMsgs]);
 
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
+      <Chats messages={messages} />
     </div>
   );
 };
